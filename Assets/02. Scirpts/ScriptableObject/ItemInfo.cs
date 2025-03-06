2c5ee7bda09c00a2d7d6349270e27004586389f8
woomay0608 <woohuck0608@naver.com>
2025-03-06 20:09:46 +0900

    [Package] 새 패키지 추가

---
+++ Assets/02. Scirpts/ScriptableObject/ItemInfo.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum ItemType
+[CreateAssetMenu(fileName = "Item", menuName = "Scriptable Object/Item")]
+public class ItemInfo : ScriptableObject
+{
+    
+    string ItemName;
+    string ItemDescrip;
+
+
+
+}
