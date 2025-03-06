261d4bb77221b916ec40fe1abb590d3e33793a56
woomay0608 <woohuck0608@naver.com>
2025-03-06 18:00:06 +0900

    [Feat] Trampoline 완성

--- Assets/02. Scirpts/Ground/Trampoline.cs
+++ Assets/02. Scirpts/Ground/Trampoline.cs
@@ -6,13 +6,21 @@ public class Trampoline : MonoBehaviour
 {
     Rigidbody body;
 
+    [Range(0, 100)] public int TrampolinePower;
+
     private void Start()
     {
         body = GetComponent<Rigidbody>();
     }
     private void OnCollisionEnter(Collision collision)
     {
-      
+        if (collision.transform.CompareTag("Player"))
+        {
+            Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 normal = collision.contacts[0].normal;
+            Vector3 Reflact = Vector3.Reflect(rb.velocity, normal);
+            rb.velocity = TrampolinePower * Reflact;
+        }
     }
 
 }
