7f1e5f9f48a6964a334d387954958926d2e5d25f
woomay0608 <woohuck0608@naver.com>
2025-03-06 12:51:15 +0900

    [Feat] 기본 세팅

---
+++ Assets/02. Scirpts/Player.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody rigidbody;
+
+    Vector3 dir;
+    public float MoveSpeed;
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 n = dir.x * transform.forward + transform.right * dir.y;
+
+    }
+
+
+    public void OnMove(InputValue value)
+    {
+        dir = value.Get<Vector2>();
+
+        Debug.Log($"{dir.x} {dir.y}");
+
+        
+ 
+    }
+
+}
