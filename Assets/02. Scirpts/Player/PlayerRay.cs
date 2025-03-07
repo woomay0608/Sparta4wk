7bfc3f88b62c05b7400791292c1780042e538221
woomay0608 <woohuck0608@naver.com>
2025-03-07 09:54:27 +0900

    [Feat] 동적 환경 조사 완료

---
+++ Assets/02. Scirpts/Player/PlayerRay.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerRay : MonoBehaviour
+{
+
+    Camera cam;
+    public LayerMask layerMask;
+
+    ItemObject itemObject;
+    [SerializeField] private TextMeshProUGUI Name;
+    [SerializeField] private TextMeshProUGUI Description;
+
+
+    private void Start()
+    {
+        cam = Camera.main;
+        Name.text = string.Empty;
+        Description.text = string.Empty; ;
+    }
+
+    private void Update()
+    {
+        Ray ray = cam.ScreenPointToRay(new Vector3(Screen.width/2, Screen.height/2));
+        Debug.DrawRay(ray.origin, ray.direction);
+        RaycastHit hit;
+        ShowItemNameAndDescriptionToRay(out hit,ray);
+
+    }
+
+
+
+    private void ShowItemNameAndDescriptionToRay(out RaycastHit hit, Ray ray)
+    {
+        if (Physics.Raycast(ray, out hit, 1f ,layerMask))
+        {
+            if (hit.transform.TryGetComponent(out itemObject))
+            {
+                Name.text = itemObject.name;
+                Description.text = itemObject.ItemDesription;
+   
+
+
+            }
+        }
+        else
+        {
+            Name.text = string.Empty;
+            Description.text = string.Empty; ;
+        }
+    }
+
+
+}
---
+++ Assets/02. Scirpts/ScriptableObject/ItemObject.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemObject : MonoBehaviour
+{
+    public ItemInfo info;
+    public string ItemName;
+    public string ItemDesription;
+    ItemType ItemType;
+
+
+    void Start()
+    {
+        ItemType = info.Type;
+        ItemName = info.ItemName;
+        ItemDesription = info.ItemDescrip;
+    }
+}
